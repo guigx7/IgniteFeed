3f340294e92c53ac677ec1d398e0a07c63c1e64b
Guilherme Abdelnor <guigo211103u@gmail.com>
2025-02-12 21:44:06 -0300

    Added global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { Header } from './Components/Header'
-import { Post } from './Post'
-import './styles.css'
-
+import { Header } from "./Components/Header";
+import { Post } from "./Post";
+import "./styles.css";
 
 function App() {
-
   return (
     <>
       <Header />
-      <Post author='Guilherme' content='AAAAAAAAAAAAAAAAAAA'/>
-      <Post author='Marcelo' content='BBBBBBBBBBBBBBB'/>
-      
+      <Post author="Guilherme" content="AAAAAAAAAAAAAAAAAAA" />
+      <Post author="Marcelo" content="BBBBBBBBBBBBBBB" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
